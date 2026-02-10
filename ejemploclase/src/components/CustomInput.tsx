725fed1b09c427f49ff1caf01d11eb79e0165cbe
Emerson Caballero <emersoncaballero2017@gmail.com>
2026-02-09 22:29:28 -0600

    validaciones logicas para comportamiendo condicionado en custom input

--- ejemploclase/src/components/CustomInput.tsx
+++ ejemploclase/src/components/CustomInput.tsx
@@ -1,72 +1,87 @@
-import { TextInput, View, Text, StyleSheet, TouchableOpacity } from "react-native";
-import{Ionicons, MaterialIcons} from "@expo/vector-icons";
+import { TextInput, View, Text, StyleSheet, TouchableOpacity, KeyboardTypeOptions } from "react-native";
+import { Ionicons, MaterialIcons } from "@expo/vector-icons";
 import { useState } from "react";
 
-type Props ={
+type Props = {
     placeholder: string,
-    onChange: (text: string) => void,
-    value: string,
-    typeInput: 'password' | 'email' | 'numeric' | 'text';
+    onChange: (text: string) => void;
+    value: string;
+    typeInput : 'password' | 'email' | 'number' | 'text';
+    
 }
 export default function CustomInput ({placeholder, onChange, value, typeInput}:Props){
 //uso de variables en el estado local
         //sintaxis:
-        //[nombreDeVariable,Funcion] = useState(<valorInicial>)
-    const [isSecureText, setIsSecureText] = useState(typeInput === 'password');
+        //[nombreDeVariable, funcion] = useState(<valorInicial>);
+    const [isSecureText, setIsSecureText] = useState(typeInput === 'password'); 
     const isPasswordField = typeInput === 'password';
 
-    const icon : typeof MaterialIcons["name"] | undefined = 
-        typeInput === "email" ? "email": 
+    const icon : typeof MaterialIcons["name"]|undefined =
+        typeInput === "email" ? "email" : 
             typeInput === "password" ? "lock" : undefined
 
+
+    const keyboardType: KeyboardTypeOptions = 
+    typeInput === "email" ? "email-address" :
+           typeInput === "number" ? "numeric" : "default" 
+           
+    const getError=()=>{
+        if(typeInput === "email" && !value.includes('@')) 
+            return 'Correo invalido';
+        if(typeInput === 'password' && value.length <6)
+            return 'Contraseña debe ser mas fuerte';
+    }
+
+    const error = getError();
+
     return(
-        //Wrapper
+        //wrapper
         <View style={styles.wrapper}>
+            {/* //inputContainer */}
             <View style={styles.inputContainer}>
-                {/*inputContainer*/}
                 <MaterialIcons 
                     name={icon}
                     size={20}
                     color={"#000000"}
                 />
-                <TextInput
+                <TextInput 
                     style={styles.input}
                     placeholder={placeholder}
-                    value={value}
+                    value={value} 
                     onChangeText={onChange}
                     secureTextEntry={isSecureText}
+                    keyboardType={keyboardType}
                 />
-                {isPasswordField &&  
-                    <TouchableOpacity onPress={()=>{setIsSecureText(!isSecureText)}}>
-                        <Ionicons name={isSecureText ? "eye" : "eye-off"} size={20}/> 
-                    </TouchableOpacity>
-                }
-                
+
+             { isPasswordField && 
+                <TouchableOpacity onPress={()=>{setIsSecureText(!isSecureText)}}>
+                    <Ionicons name={isSecureText ? "eye" : "eye-off"}  size={20} />
+                </TouchableOpacity> 
+            }
+            
             </View>
-            <Text>*Campo Requerido</Text>
+            {error && <Text> {error} </Text>}
         </View>
     );
-
 }
 
 const styles = StyleSheet.create({
     wrapper:{
         marginBottom:10,
-        width: "100%"
+        width: "100%",
+        paddingHorizontal: 25,
     },
     inputContainer:{
         flexDirection:'row',
         alignItems:'center',
-
+        
         borderWidth: 1,
         borderColor: "#ccc",
-        borderRadius: 6,
-        paddingHorizontal: 15,
+        borderRadius: 8,
+        paddingHorizontal: 13,
     },
     input:{
         paddingHorizontal:10,
-        width:'80%',
+        width:'80%'
     }
-
-
 });
